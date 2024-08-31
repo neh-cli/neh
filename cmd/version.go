3a713fa9712da961dc4e3600ab695202b7621247
Atsushi Ishida <gipcompany@users.noreply.github.com>
2024-08-31 14:58:19 +0900

    First Cli App (#2)
    * .go-version
    
    * ```
    $ go mod init github.com/gipcompany/neh
    go: creating new go.mod: module github.com/gipcompany/neh
    ```
    
    * cobra-cli init
    
    * ```
    $ tree
    .
    |-- cmd
    |   `-- root.go
    |-- LICENSE
    |-- README.md
    |-- go.mod
    |-- go.sum
    `-- main.go
    
    2 directories, 6 files
    ```
    
    * cobra-cli add version
    
    * ```
    $ tree
    .
    |-- cmd
    |   |-- root.go
    |   `-- version.go
    |-- LICENSE
    |-- README.md
    |-- go.mod
    |-- go.sum
    `-- main.go
    
    2 directories, 7 files
    ```
    
    * update version cmd
    
    * ```
    $ go run main.go version
    Version of neh command: 0.0.1
    ```

---
+++ cmd/version.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Define the version information
+const Version = "0.0.1"
+
+// versionCmd represents the `neh version` command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display the version information",
+	Long: `The version command displays the current version of the neh command.
+It provides detailed information about the version number, which can be useful for troubleshooting and ensuring compatibility.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version of neh command: %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
