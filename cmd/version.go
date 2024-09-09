bb1d601d1b909bdfb157b2fb68f3595a68e30ead
Atsushi Ishida <gipcompany@gmail.com>
2024-09-10 01:31:51 +0900

    v0.0.3 release

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Define the version information
-const Version = "0.0.1"
+const Version = "0.0.3"
 
 // versionCmd represents the `neh version` command
 var versionCmd = &cobra.Command{
