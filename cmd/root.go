1e76931214a3f886c1a7d2ef02afaeb974acbe61
Atsushi Ishida <gipcompany@gmail.com>
2024-09-15 10:32:17 +0900

    docs: Improving the Help Subcommand in Goreleaser

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -15,16 +11,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "neh",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "neh is a fast CLI app for real-time interaction with AI specializing in Large Language Models.",
+	Long: `neh is a sleek CLI application designed for high-speed,
+real-time interaction with AI specializing in Large Language Models.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +34,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
