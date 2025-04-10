673601d4e586f62c84ffe40b7d124b6beee2c0b3
Atsushi Ishida <gipcompany@gmail.com>
2025-04-11 02:07:56 +0900

    todo command

---
+++ cmd/todo.go
@@ -0,0 +1,37 @@
+// File: cmd/todo.go
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/atotto/clipboard"
+	"github.com/neh-cli/neh/cmd/shared"
+	"github.com/spf13/cobra"
+)
+
+var todoCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "This command sends text containing TODO to an AI and asks for proposed solutions to the TODO.",
+	Run:   runTodoCmd,
+}
+
+func init() {
+	rootCmd.AddCommand(todoCmd)
+}
+
+func runTodoCmd(cmd *cobra.Command, args []string) {
+	clipboardMessage, err := clipboard.ReadAll()
+
+	if err != nil {
+		fmt.Println("Operation aborted because clipboard contents could not be retrieved.", err)
+		return
+	}
+
+	queryMessage := ""
+	err = shared.ExecuteWebSocketCommand("todo", queryMessage, clipboardMessage)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
